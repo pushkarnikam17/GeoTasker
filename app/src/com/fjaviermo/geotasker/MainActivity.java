177d39874af2575e33ccf482c655123abe2eb68f
Francisco Javier Martin Otero <fjavier.mo@gmail.com>
2013-11-26 19:01:22 +0100

    Created menus to each fragment

--- app/src/com/fjaviermo/geotasker/MainActivity.java
+++ app/src/com/fjaviermo/geotasker/MainActivity.java
@@ -1,23 +1,57 @@
 package com.fjaviermo.geotasker;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
 
-public class MainActivity extends Activity {
+import com.fjaviermo.dao.ProfilesDataSource;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+public class MainActivity extends Activity 
+implements OnSelectedProfileChangeListener {
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
+		ProfilesDataSource profileDAO = new ProfilesDataSource(getBaseContext());
+		profileDAO.open();
+		profileDAO.close();
+		
+		// Get the profile description fragment
+		FragmentManager fm = getFragmentManager();
+
+		// Begin transaction
+		FragmentTransaction ft = fm.beginTransaction();
+
+		// Create the Fragment and add
+		ProfileListFragment listFragment = new ProfileListFragment();
+		ft.add(R.id.layoutRoot, listFragment, "profileList");
+
+		// Commit the changes
+		ft.commit();
+	}
+	
+	@Override
+	public void onSelectedProfileChanged(long profileIndex) {
+		ProfileFragment profileDescFragment;
+		FragmentManager fm = getFragmentManager();
+
+		// Handle dynamic switch to description fragment
+		FragmentTransaction ft = fm.beginTransaction();
+
+		// Create the fragment and attach profile id
+		profileDescFragment = new ProfileFragment();
+		Bundle args = new Bundle();
+		args.putLong(ProfileFragment.PROFILE_INDEX, profileIndex);
+		profileDescFragment.setArguments(args);
+
+		// Replace the profile list with the description
+		ft.replace(R.id.layoutRoot, profileDescFragment, "profileDescription");
+		ft.addToBackStack(null);
+		ft.setCustomAnimations(
+				android.R.animator.fade_in, android.R.animator.fade_out);
+		ft.commit();
+	}
 }
